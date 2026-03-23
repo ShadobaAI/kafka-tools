1fbe3470810ad09c1e6d7b8e2bd77ce9d85441b7
ShadobaAI <aleks.p1799@gmail.com>
2026-03-23 23:52:04 +0300

    fix push-dt

--- docker-image/push-dt.ps1
+++ docker-image/push-dt.ps1
@@ -1,15 +1,18 @@
 $FILE  = ".\template.dt"
 
-$OWNER  = ("ghcr.io" | docker-credential-desktop get | ConvertFrom-Json).Username
+$CREDS  = "ghcr.io" | docker-credential-desktop get | ConvertFrom-Json
+$OWNER  = $CREDS.Username
 $IMAGE  = "ghcr.io/$OWNER/kfk-tmpl-dt:latest"
 
 # Логин в GHCR
-$TOKEN | oras login ghcr.io --username $OWNER --password-stdin
+$CREDS.Secret | oras login ghcr.io --username $OWNER --password-stdin
 
 # Публикация артефакта
+Push-Location (Split-Path $FILE)
 oras push $IMAGE `
     --artifact-type "application/vnd.1c.dt" `
     --disable-path-validation `
-    "${FILE}:application/octet-stream"
+    "$(Split-Path $FILE -Leaf):application/octet-stream"
+Pop-Location
 
 Write-Host "Pushed: $IMAGE"
